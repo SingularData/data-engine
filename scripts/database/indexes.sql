7707846876452513315c7fa21d4491d73116865a
haoliangyu <njkon@foxmail.com>
2017-04-04 00:37:54 -0400

    remove database schema

--- scripts/database/indexes.sql
+++
@@ -1,6 +0,0 @@
-CREATE INDEX ON dataset (updated_time);
-CREATE INDEX ON dataset (portal_dataset_id);
-CREATE UNIQUE INDEX ON dataset (portal_id, portal_dataset_id, updated_time);
-CREATE UNIQUE INDEX ON dataset_tag (name);
-CREATE UNIQUE INDEX ON dataset_category (name);
-CREATE UNIQUE INDEX ON dataset_publisher (name);
