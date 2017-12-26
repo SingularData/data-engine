141cbc05b1aaa27e64b60d508739367cac1b21e8
haoliangyu <njkon@foxmail.com>
2017-12-26 00:07:29 -0500

    add log message

--- src/bootstrapper/index.ts
+++ src/bootstrapper/index.ts
@@ -3,6 +3,8 @@ import AWS = require("aws-sdk");
 AWS.config.region = "us-east-1";
 
 exports.handler = (event, context) => {
+  console.log("Start publishing data request tasks.");
+
   const s3 = new AWS.S3();
   const sns = new AWS.SNS();
 
@@ -19,6 +21,8 @@ exports.handler = (event, context) => {
       const tasks = [];
 
       for (let source of list) {
+        console.log(`Publishing request task for data source ${source.name}.`);
+
         const task = sns
           .publish({
             Message: JSON.stringify(source),
@@ -26,13 +30,21 @@ exports.handler = (event, context) => {
             TopicArn: process.env.SNS_REQUEST_QUEUE
           })
           .promise()
-          .catch(err => console.error(err));
+          .then(() =>
+            console.log(
+              `Published request task for data source ${source.name}.`
+            )
+          )
+          .catch(err => {
+            console.log(`Failed to publish task for ${source.name}.`);
+            console.error(err);
+          });
 
         tasks.push(task);
       }
 
       return Promise.all(tasks);
     })
-    .then(() => context.done(null, "Function Finished!"))
+    .then(() => context.done(null, "Request task publication finished!"))
     .catch(err => context.done(err));
 };
