0221964112fc6a3e56a611fdd1b8b9a9aca41b08
haoliangyu <njkon@foxmail.com>
2017-12-31 20:52:23 -0500

    remove dist

--- dist/fetcher/hash-util.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const crypto_1 = require("crypto");
-function sha256(data) {
-    const hash = crypto_1.createHash("sha256");
-    hash.update(data);
-    return hash.digest("base64");
-}
-exports.sha256 = sha256;
